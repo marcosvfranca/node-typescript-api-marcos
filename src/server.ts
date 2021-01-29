998592af9b5431aa94edd3b60554a635f5d65b5a
Marcos <marcos-vdm@hotmail.com>
2021-01-29 11:06:55 -0300

    final commit

---
+++ src/server.ts
@@ -0,0 +1,53 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
+import { Application } from 'express';
+import * as database from '@src/database';
+import { UsersController } from './controllers/users';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupController();
+    await this.databaseSetup();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    const beachesController = new BeachesController();
+    const usersController = new UsersController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController
+    ]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port: ', this.port);
+    });
+  }
+}
---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,128 @@
+import { Beach, BeachPosition } from "@src/models/beach";
+import nock from 'nock';
+import stormGlassWeather3HoursFixtures from '@test/fixtures/stormflass_weather_3_hours.json';
+import { User } from "@src/models/user";
+import AuthService from "@src/services/auth";
+
+describe('Beach forecast functional tests', () => {
+  const defaultUser = {
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: '1234',
+  };
+  let token: string;
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    await User.deleteMany({});   
+    const user = await new User(defaultUser).save();
+    const defaultBeach = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.E,
+      user: user.id
+    };
+    await new Beach(defaultBeach).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
+
+  it('should return a forecast with just a few times', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: -33.792726,
+        lng: 151.289824,
+        params: /(.*)/,
+        source: 'noaa'
+      })
+      .reply(200, stormGlassWeather3HoursFixtures);
+
+    const { body, status } = await global.testRequest.get('/forecast').set({ 'x-access-token': token });
+    expect(status).toBe(200);
+    expect(body).toEqual([
+      {
+        time: '2020-04-26T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 3.89,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 0.47,
+            windDirection: 299.45,
+            windSpeed: 100
+          },
+        ],
+      },
+      {
+        time: '2020-04-26T01:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2020-04-26T01:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100
+          },
+        ],
+      },
+      {
+        time: '2020-04-26T02:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 182.56,
+            swellHeight: 0.28,
+            swellPeriod: 3.44,
+            time: '2020-04-26T02:00:00+00:00',
+            waveDirection: 232.86,
+            waveHeight: 0.46,
+            windDirection: 321.5,
+            windSpeed: 100,
+          },
+        ],
+      }
+    ]);
+  });
+  
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v1/weather/point')
+      .query({ lat: '-33.792726', lng: '151.289824' })
+      .replyWithError('Something went wrong');
+
+    const { status } = await global.testRequest.get(`/forecast`).set({ 'x-access-token': token });
+
+    expect(status).toBe(500);
+  });
+});
